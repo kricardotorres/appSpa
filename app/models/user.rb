3d7d9cf589a1a5d7251933ad9605294158cc95be
ricardo medina <ricardomedin@outlook.com>
2019-08-14 14:57:28 -0500

    previsualization on foliosda

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,  
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :registerable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 end
